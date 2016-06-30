c0c48d628bf34260114a51e6a9b2681006ad14ac
Shota Iguchi <shota-iguchi@cookpad.com>
2016-06-30 10:18:37 +0900

    Add Vundle.vim submodule

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@ DOTPATH=~/.dotfiles
 
 for file in .??*
 do
-    [ "$file" == ".git" ] || [ "$file" == ".gitignore" ] || [ "$file" == ".DS_Store" ] && continue
-
+    [ "$file" == ".git" ] || [ "$file" == ".gitignore" ] || [ "$file" == ".gitmodules" ] || [ $file == ".DS_Store" ] && continue
     ln -snfv "$DOTPATH/$file" "$HOME/$file"
 done

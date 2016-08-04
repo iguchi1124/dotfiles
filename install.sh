604346096c6b508cf8dc74bcc57d0c8a908574a7
Shota Iguchi <e.iguchi1124@gmail.com>
2016-08-04 14:29:43 +0900

    Modify coding style

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
-DOTPATH=~/.dotfiles
+DOTPATH=$HOME/.dotfiles
 
-for file in .??*
-do
+for file in .??*; do
     [ "$file" == ".git" ] || [ "$file" == ".gitignore" ] || [ "$file" == ".gitmodules" ] || [ $file == ".DS_Store" ] && continue
     ln -snfv "$DOTPATH/$file" "$HOME/$file"
 done
